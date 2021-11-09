85978ea5afb9d0da28cf76f5f130e1a915b92804
bum <sb910126@gmail.com>
2021-11-10 02:43:43 +0900

    modified rebarCalc.js

--- rebarCalc.js
+++ rebarCalc.js
@@ -9,17 +9,17 @@ console.log('What is the price of a sash per feet?')
 console.log('Please enter a value')
 const perFeet = readlineSync.question('what is the feet?')
 
-
-//300cm * 200cm
+//가로, 세로 입력 받기
 const width = readlineSync.question('what is the width?')
 const height = readlineSync.question('what is the height?')
 
 // 창문 => 가로 * 2, 세로 * 2 전체 샤시 길이
-const totalLenght = (width * 2) + (height * 2)
+// m 단위로만 판매하기 때문에 0.01을 곱해서 cm -> m 단위 변경
+const totalLenght = ((width * 2) + (height * 2)) * 0.01
 
-// 길이 cm -> ft
-//m 단위로만 판매한다
-const totalFeet = Math.ceil(totalLenght / 30.48)
+// 길이 m -> ft
+// 1ft = 0.3048
+const totalFeet = Math.ceil(totalLenght / 0.3048)
 
 // 가격 계산
 const totalpay  = parseInt(totalFeet * perFeet)
