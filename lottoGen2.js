0dd3a55f6a44c909a56c6589f91868d2e2355e19
bum <sb910126@gmail.com>
2021-11-10 02:39:14 +0900

    modified arrInfo.js

--- lottoGen2.js
+++ lottoGen2.js
@@ -7,9 +7,12 @@ for (let i = 1; i <= 45; i++) { //1~45
 
 }
 
-//6개 공뽑기 배열에서
+//6개 공뽑기
 for (let i = 0; i < 6; i++) {
-    const idx = parseInt(Math.random() * arr.length) //1.45 44 43
+    const idx = parseInt(Math.random() * arr.length)+1
+    //1. arr.length === 45
+    //2. arr.length === 44
+    //3. arr.length === 43 ...
     arr.splice(idx, 1)
 
     console.log(arr)
