a28f2ac16a7dbdbb0cb23ddfab676d0db30b54b2
bum <sb910126@gmail.com>
2021-11-10 02:14:57 +0900

    modified lottoGen.js

--- lottoGen.js
+++ lottoGen.js
@@ -2,15 +2,15 @@ const arr = []
 
 while(true) {
     //숫자를 생성 1~45
-    const num = parseInt(Math.random() * 45) + 1 //0~1미만의 난수 0.9999 * 45 = 45.999
+    const num = parseInt(Math.random() * 45) + 1 //0~1미만의 난수 0.9999 * 44 = 44.999
 
-    //검사
+    //중복되는 수 있는지 검사, 없으면 -1
     const index = arr.indexOf(num)
 
     console.log(`NUM: ${num} INDEX: ${index}`)
 
-    if(index >= 0){
-        continue //바로 다시시작
+    if(index >= 0){//중복되는 수가 있는 경우 다시 숫자 생성
+        continue
     }
     const newLenght = arr.push(num)
 
