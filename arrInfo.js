f6c3fa5b8eecfd11a2634b7e3a86431cccd5c3d0
bum <sb910126@gmail.com>
2021-11-10 09:09:38 +0900

    modified arrInfo.js

--- arrInfo.js
+++ arrInfo.js
@@ -3,15 +3,18 @@ console.log('arr create')
 let fruits = ['사과', '바나나']
 console.log(fruits)
 
-// 에러발생 예문
-//const fruits = ['사과', '바나나', '수박', '참외']
-//console.log(fruits.length)
+const fruits1 = ['사과', '바나나', '수박', '참외']
+console.log(fruits.length)
 
+// 에러발생 예문
 // const MY_ARRAY = [];
 // // 배열에 아이템을 삽입하는 건 가능합니다
 // MY_ARRAY.push('A'); // ["A"]
 // // 하지만 변수에 새로운 배열을 배정하면 에러가 발생합니다
 // MY_ARRAY = ['B']
+//에러발생 새로운 배열 배정
+//const fruits1 = ['사과', '바나나', '수박', '참외','포도']
+//console.log(fruits.length)
 
 
 //배열추가
