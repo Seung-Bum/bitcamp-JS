b80789f963504b29dfd78ca6f5ae503a2051e5bc
bum <sb910126@gmail.com>
2021-11-10 22:41:16 +0900

    modified README.md, sortEx.js, funEx.js

--- sortEx.js
+++ sortEx.js
@@ -1,3 +1,4 @@
+//정렬은 음수, 양수 아니면 0밖에없다.
 const arr = [4, 3,21,43,77,22,11,45]
 
 arr.sort((a,b) => {
@@ -5,7 +6,10 @@ arr.sort((a,b) => {
     a-b
 
 })
-//정렬은 음수 양수 아니면 0밖에없다.
-
 console.log(arr)
 
+
+arr.sort(function (a, b){
+    return b-a
+})
+console.log(arr)
