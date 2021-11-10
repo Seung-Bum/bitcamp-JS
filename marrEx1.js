f3317f337d9ae212094a02d13f5c47d5114b4296
bum <sb910126@gmail.com>
2021-11-10 12:45:51 +0900

    Create marrEx1.js
    Multiple Array Exercises

--- marrEx1.js
+++ marrEx1.js
@@ -8,24 +8,35 @@ const arr =[
 
 // 가장 매출이 좋았던 분기
 // 각 분기별로 매출의 평균 만들기
-let sum = 0
-let avg = 0
+const parr = []
 for (let i = 0; i < arr.length; i++) {
     const tempArr = arr[i]
+
+    let sum = 0
+    let avg = 0
+
     for (let j = 0; j < tempArr.length; j++) {
         sum += tempArr[j]
-        avg = (sum / tempArr.length).toFixed(2)
+        avg = (sum / tempArr.length).toFixed(2) //toFixed하면 문자열로 바뀜
     }
     console.log(tempArr)
     console.log(avg)
+    parr.push(parseFloat(avg)) //다시 숫자로 변경
 }
-//각분기 평균 저장하는 공간이 필요하다.
 
+// 그 중에서 최대 평균 찾기
+// 각분기 평균 저장하는 공간이 필요하다. 어디에 선언을 해야하는가?
+// 유지해야 하는 데이터는 바깥쪽
 
+//최고 매출금액
+let max = 0
+let period = 0
 
-// 극 중에서 최대 평균 찾기
-
-
-//최고 매출을 찍은 월
-
-//
+for (let i = 0; i < parr.length; i++) {
+    if(parr[i] > max){
+        max = parr[i]
+        period = i
+    }
+}
+console.log("max: " + max)
+console.log("period: " + (period + 1) + "분기입니다.")
