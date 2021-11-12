5329ef2b7a8f0dd75af8318f4148bf0b80ccdede
bum <sb910126@gmail.com>
2021-11-12 09:19:12 +0900

    20211111 Class content

--- closureEx.js
+++ closureEx.js
@@ -1,14 +1,31 @@
 //클로저 예문
 
-var base = 'Hello, ';
-function sayHelloTo(name) {
-    var text = base + name;
+// var base = 'Hello, ';
+// function sayHelloTo(name) {
+//     var text = base + name;
+//     return function() {
+//         console.log(text);
+//     };
+// }
+//
+// var hello1 = sayHelloTo('승민');
+// var hello2 = sayHelloTo('현섭');
+// var hello3 = sayHelloTo('유근');
+// hello1(); // 'Hello, 승민'
+// hello2(); // 'Hello, 현섭'
+// hello3(); // 'Hello, 유근'
+
+
+var initial = 0
+function sayHelloTo(money) {
+    var savings = initial + money
     return function() {
-        console.log(text);
+        savings += 5000
+        console.log(savings)
     };
 }
 
-var hello1 = sayHelloTo('승민');
+var hello1 = sayHelloTo(5000);
 var hello2 = sayHelloTo('현섭');
 var hello3 = sayHelloTo('유근');
 hello1(); // 'Hello, 승민'
