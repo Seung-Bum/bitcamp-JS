e00ac6b28d8887b70c9a3e846e27d77b5c84749d
bum <sb910126@gmail.com>
2021-11-18 22:34:50 +0900

    Map review create ex4_1 needs confirmation

--- ex4_1.js
+++ ex4_1.js
@@ -2,10 +2,7 @@ const storeService = (function (){
 
     let storeArr = []
 
-    //반드시 있어야 할 기능 -> JSON 데이터 가져오기
     function loadData(url, callback){
-
-        //fetch 데이터를 읽어옴, 비동기, 외부에서 가져옴 자바스크립트에서
         fetch(url)
             .then(  response => response.json())
             .then(  data => {
@@ -15,5 +12,11 @@ const storeService = (function (){
             })
     }
 
-    return {loadData}
+    function getDataByIndex(index){
+
+        return storeArr[index]
+    }
+
+    return {loadData, getDataByIndex}
+
 })()
