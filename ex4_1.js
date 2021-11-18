6cecf32c1876ec32f5ef939ad1e6fc905e803598
bum <sb910126@gmail.com>
2021-11-19 00:17:16 +0900

    ex4_1.html
    Add a comment

--- ex4_1.js
+++ ex4_1.js
@@ -6,7 +6,7 @@ const storeService = (function (){
         fetch(url)
             .then(  response => response.json())
             .then(  data => {
-                //console.log(data)
+                console.log(data)
                 storeArr = data
                 callback(data)
             })
