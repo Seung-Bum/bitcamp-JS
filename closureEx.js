86610556de553005d4d4abed96196867d3e2319e
Seung-Bum <sb910126@gamil.com>
2021-12-05 23:15:16 +0900

    클로저 예문 확인

--- closureEx.js
+++ closureEx.js
@@ -1,5 +1,6 @@
 //클로저 예문
 
+// 블로그 예문
 // var base = 'Hello, ';
 // function sayHelloTo(name) {
 //     var text = base + name;
@@ -16,18 +17,26 @@
 // hello3(); // 'Hello, 유근'
 
 
-var initial = 0
-function sayHelloTo(money) {
-    var savings = initial + money
-    return function() {
-        savings += 5000
+//수업시간 돼지저금통 예문
+function save() {
+
+    let savings = 0
+
+    return function add(money) {
+        savings += money
         console.log(savings)
-    };
+    }
+
 }
 
-var hello1 = sayHelloTo(5000);
-var hello2 = sayHelloTo('현섭');
-var hello3 = sayHelloTo('유근');
-hello1(); // 'Hello, 승민'
-hello2(); // 'Hello, 현섭'
-hello3(); // 'Hello, 유근'
+const s1 = save()
+s1(5000)
+s1(1000)
+s1(2000)
+
+console.log('-------------------------------------')
+
+const s2 = save()
+s2(3000)
+s2(3000)
+s2(3000)
